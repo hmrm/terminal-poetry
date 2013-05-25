6d62ce5fcf0e9526df50da389b22a5cbad0347f9
Haney Maxwell <hmrmaxwell@gmail.com>
2013-05-24 19:47:10 -0500

    inital commit, generation of json object with poems

---
+++ parsable_to_json.py
@@ -0,0 +1,29 @@
+import sys
+import json
+
+lines = [line.strip() for line in sys.stdin.readlines()]
+
+poems = []
+curpoem = []
+
+for line in lines:
+    if line == '':
+        if curpoem != []:
+            poems.append(curpoem)
+        curpoem = []
+    else:
+        curpoem.append(line)
+
+poems.append(curpoem)
+
+output = []
+for poem in poems:
+    info = { "Japanese" : {}, "English" : {}}
+    length = len(poem) / 2
+    info["Japanese"]["Author"] = poem[0]
+    info["English"]["Author"] = poem[length]
+    info["Japanese"]["Text"] = poem[1:length]
+    info["English"]["Text"] = poem[length + 1:]
+    output.append(info)
+
+print json.dumps(output)
